d0eb62adc91118b6912ab37bd0eadbdbfed538a3
Mikael O. Bonnier <mikael.bonnier@gmail.com>
2017-07-09 18:16:57 +0200

    Updated temperature.c.

--- temperature.c
+++ temperature.c
@@ -27,41 +27,29 @@ int main ()
   int x, y;
   int txtwidth;
 
-  // https://www.raspberrypi.org/forums/viewtopic.php?f=33&t=170112
-  float systemp, millideg;
-  FILE *thermal;
-  int n;
-  static char s[50];
-
   /* open the display (connect to the X server) */
   dpy = XOpenDisplay (getenv ("DISPLAY"));
-
 
   /* get the root window */
   root = DefaultRootWindow (dpy);
 
-
   /* get attributes of the root window */
   XGetWindowAttributes(dpy, root, &wa);
 
-
   /* create a GC for drawing in the window */
   g = XCreateGC (dpy, root, 0, NULL);
-
 
   /* load a font */
   // f = XLoadFont(dpy, "-*-fixed-*-r-*-*-13-*-*-*-*-*-iso8859-*"); // Only font that works in VcXsrv 1.19.2.0
   f = XLoadFont(dpy, "-*-helvetica-bold-r-*-*-*-240-*-*-*-*-iso8859-*");
   XSetFont(dpy, g, f);
 
-
   /* get font metrics */
   XGetGCValues (dpy, g, GCFont, &v);
   fs = XQueryFont (dpy, v.font);
 
-
   /* allocate colors */
-  for(c = 0; c < NCOLORS; ++c) {
+  for (c = 0; c < NCOLORS; ++c) {
     XAllocNamedColor(dpy,
                      DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)),
                      colors[c],
@@ -69,7 +57,12 @@ int main ()
     xcolors[c] = sc;
   }
 
-
+  // Read CPU-temperature in C.
+  // https://www.raspberrypi.org/forums/viewtopic.php?f=33&t=170112
+  float systemp, millideg;
+  FILE *thermal;
+  int n;
+  static char s[50];
   float systemp_save = 60.00f;
   /* draw something */
   for(;;) {
